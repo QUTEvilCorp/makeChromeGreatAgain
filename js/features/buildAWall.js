9d7333a571534f69a4f7f82adbc2e79b6aeecff4
Simon Gordon <gordonsimon@hotmail.com>
2016-08-14 14:17:42 +1000

    I built a wall and made JavaScript pay for it (using canvas instead of image)

--- js/features/buildAWall.js
+++ js/features/buildAWall.js
@@ -1,14 +1,32 @@
 function buildAWall() {
-    document.body.innerHTML += "<div style='position:fixed;"
-                            +      " bottom:77px;"
-                            +      " width:100%;"
-                            +      " height:77px;"
-                            +      " background-color: antiquewhite;'>"
-                            +      "<img src='"
-                            +          chrome.extension.getURL("img/wall.jpg")
-                            +          "'"
-                            +          " width='100%'"
-                            +          " />"
-                            +  "</div>";
+    if (document.body.innerHTML.match(/([tT]rump|[mM]exic(o|an))/)){
+        document.body.innerHTML += '<canvas id="canvas" class="wall" width="5000" height="150">';
+
+        function drawBrick(context, brickNum, y){
+            //Whole function can be cleaned up a bit
+           var width = 40;
+           var height = 20;
+           var xPos = brickNum * width - (y % 2 ? width / 2 : 0);
+           var yPos = y * height;
+           var colours = ["red", "orange", "#ff6633", "#b32d00", "#cc3300", "#e63900", "#e60000", "#e67300"];
+           var col = colours[Math.floor(Math.random()*colours.length)];
+           context.fillStyle = col;
+           context.fillRect(xPos, yPos, width, height);
+           context.lineWidth = 2;
+           context.strokeStyle = "black";
+           context.strokeRect(xPos, yPos, width, height);
+        }
+
+        my_canvas = document.getElementById("canvas");
+        context = my_canvas.getContext("2d");
+
+        context.beginPath();
+        //Could be improved.
+        for (var y = 0; y < 100; y++){
+            for (var x = 0; x < 1000; x++){
+                drawBrick(context, x, y);
+            }
+        }
+    }
 }
 
