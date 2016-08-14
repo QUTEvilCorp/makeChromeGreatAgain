16c132f98471e9d14fc04194567bf1d13472b4bd
Simon Gordon <gordonsimon@hotmail.com>
2016-08-14 16:49:18 +1000

    tweaking

--- js/features/rightToLeft.js
+++ js/features/rightToLeft.js
@@ -1,7 +1,7 @@
 // Make text appear normal, but it's actually in right-to-left
 function rightToLeft() {
     var defaultFreq = 0.5;
-    $("h1, h2, h3, h4, h5, h6, p, span, div").each(function(){
+    $("h1, h2, h3, h4, h5, h6, p, div").each(function(){
         // If the only child of this is text (so it doesn't break tags underneath)
         if ($(this).contents().length == 1 && $(this).contents()[0].nodeType == 3){
             if (Math.random() <= defaultFreq) {
